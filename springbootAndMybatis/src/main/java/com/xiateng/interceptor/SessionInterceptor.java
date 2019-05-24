ec4728218ccebef0f8bc62115e63eb3bf35c4ad3
xiateng <1296513495@qq.com>
2019-05-24 13:24:52 +0800

    sss

--- springbootAndMybatis/src/main/java/com/xiateng/interceptor/SessionInterceptor.java
+++ springbootAndMybatis/src/main/java/com/xiateng/interceptor/SessionInterceptor.java
@@ -17,6 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 public class SessionInterceptor implements HandlerInterceptor{
     private static final Logger logger = LoggerFactory.getLogger(SessionInterceptor.class);
 
+    /**
+     * 进入controller之前会调用该方法，
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 登录不做拦截
@@ -37,11 +45,27 @@ public class SessionInterceptor implements HandlerInterceptor{
         return true;
     }
 
+    /**
+     * 请求处理之后进行调用，但是在视图渲染之前（controller方法调用之后）
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
         logger.info("---postHandle被调用------");
     }
 
+    /**
+     * 在整个请求结束之后被调用，也就是在DispatcherServlet渲染了对应的视图之后执行
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
         logger.info("---afterCompletion被调用------");
